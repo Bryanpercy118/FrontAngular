ffeb1202bf380ba3249374a853e400be05f70ee0
bryanpercy118 <bryanpercy77@gmail.com>
2024-04-26 16:09:28 -0500

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListProductsComponent } from './components/list-products/list-products.component';
+import { AddEditProductsComponent } from './components/add-edit-products/add-edit-products.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component:ListProductsComponent},
+  {path: 'add', component:AddEditProductsComponent},
+  {path:'**',redirectTo:'', pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
