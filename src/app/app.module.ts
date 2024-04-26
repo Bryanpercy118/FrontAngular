1b30d84d21e37828852ffb0b95c39331504f6a13
bryanpercy118 <bryanpercy77@gmail.com>
2024-04-26 15:17:50 -0500

    more components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { ListProductsComponent } from './components/list-products/list-products.component';
+import { AddEditProductsComponent } from './components/add-edit-products/add-edit-products.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    ListProductsComponent,
+    AddEditProductsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/add-edit-products/add-edit-products.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-edit-products',
+  templateUrl: './add-edit-products.component.html',
+  styleUrl: './add-edit-products.component.css'
+})
+export class AddEditProductsComponent {
+
+}
