ae587eb1a67b70a562c078b0135ee38ee4a96020
bryanpercy118 <bryanpercy77@gmail.com>
2024-04-26 15:50:41 -0500

    views

--- src/app/components/list-products/list-products.component.ts
+++ src/app/components/list-products/list-products.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
-
+import { Product } from '../../interfaces/product';
 @Component({
   selector: 'app-list-products',
   templateUrl: './list-products.component.html',
   styleUrl: './list-products.component.css'
 })
 export class ListProductsComponent {
+  listProducts: Product[] = [
+    {
+      name:'Coca cola',
+      description: 'Gaseosa',
+      price: 10,
+      stock: 100
+    },
+    {
+      name:'Pepsi',
+      description: 'Gaseosa',
+      price: 10,
+      stock: 100
+    }
 
+  ];
 }
